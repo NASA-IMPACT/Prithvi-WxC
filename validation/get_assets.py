bfeb653b858a71759139f77a584eabdd3e31ea5a
Johannes Schmude <Johannes.Schmude@ibm.com>
2025-03-24 17:15:22 -0400

    Fixing paths.

--- validation/get_assets.py
+++ validation/get_assets.py
@@ -45,28 +45,28 @@ def get_model_data():
 
     surf_in_scal_path = Path("./climatology/musigma_surface.nc")
     hf_hub_download(
-        repo_id="ibm-nasa-geospatial/Prithvi-WxC/prithvi.wxc.2300m.v1",
+        repo_id="ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M",
         filename=f"climatology/{surf_in_scal_path.name}",
         local_dir="data",
     )
 
     vert_in_scal_path = Path("./climatology/musigma_vertical.nc")
     hf_hub_download(
-        repo_id="ibm-nasa-geospatial/Prithvi-WxC/prithvi.wxc.2300m.v1",
+        repo_id="ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M",
         filename=f"climatology/{vert_in_scal_path.name}",
         local_dir="data",
     )
 
     surf_out_scal_path = Path("./climatology/anomaly_variance_surface.nc")
     hf_hub_download(
-        repo_id="ibm-nasa-geospatial/Prithvi-WxC/prithvi.wxc.2300m.v1",
+        repo_id="ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M",
         filename=f"climatology/{surf_out_scal_path.name}",
         local_dir="data",
     )
 
     vert_out_scal_path = Path("./climatology/anomaly_variance_vertical.nc")
     hf_hub_download(
-        repo_id="ibm-nasa-geospatial/Prithvi-WxC/prithvi.wxc.2300m.v1",
+        repo_id="ibm-nasa-geospatial/Prithvi-WxC-1.0-2300M",
         filename=f"climatology/{vert_out_scal_path.name}",
         local_dir="data",
     )
